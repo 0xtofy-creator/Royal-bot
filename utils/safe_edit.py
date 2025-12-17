cf85bb7c27b2230b8975fe60eaa0ff86c762c3c8
0xtofy-creator <0xtofy@gmail.com>
2025-12-17 03:11:34 -0800

    Update safe_edit.py

--- utils/safe_edit.py
+++ utils/safe_edit.py
@@ -2,11 +2,7 @@ from aiogram.exceptions import TelegramBadRequest
 
 
 async def safe_edit_text(message, text: str, reply_markup=None) -> None:
-    """
-    Telegram запрещает edit_text, если текст и reply_markup не меняются.
-    В таком случае TelegramBadRequest: message is not modified.
-    Мы это считаем нормой и гасим.
-    """
+   
     try:
         await message.edit_text(text, reply_markup=reply_markup)
     except TelegramBadRequest as e:
